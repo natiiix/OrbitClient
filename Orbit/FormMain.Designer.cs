d9cb006b411d94a4ef540fbafdc943260add144e
Ivo Meixner <ivo.meixner@se7en.cz>
2017-06-01 23:30:50 +0200

    Working on asynchronous server communication

---
+++ Orbit/FormMain.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Orbit
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.labelLatency = new System.Windows.Forms.Label();
+            this.timerUpdate = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(12, 63);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(749, 31);
+            this.textBoxInput.TabIndex = 0;
+            this.textBoxInput.TextChanged += new System.EventHandler(this.textBoxInput_TextChanged);
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(13, 100);
+            this.textBoxOutput.Multiline = true;
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.ReadOnly = true;
+            this.textBoxOutput.Size = new System.Drawing.Size(749, 207);
+            this.textBoxOutput.TabIndex = 1;
+            // 
+            // labelLatency
+            // 
+            this.labelLatency.AutoSize = true;
+            this.labelLatency.Location = new System.Drawing.Point(12, 495);
+            this.labelLatency.Name = "labelLatency";
+            this.labelLatency.Size = new System.Drawing.Size(88, 25);
+            this.labelLatency.TabIndex = 2;
+            this.labelLatency.Text = "Latency";
+            // 
+            // timerUpdate
+            // 
+            this.timerUpdate.Tick += new System.EventHandler(this.timerUpdate_Tick);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(774, 529);
+            this.Controls.Add(this.labelLatency);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.textBoxInput);
+            this.Name = "FormMain";
+            this.Text = "Orbit";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.Label labelLatency;
+        private System.Windows.Forms.Timer timerUpdate;
+    }
+}
+
---
+++ Orbit/FormMain.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NatiTools.xWeb;
+using NatiTools.xCSharp;
+
+namespace Orbit
+{
+    public partial class FormMain : Form
+    {
+        private const string SERVER_URL = "http://orbit.6f.sk";
+        private const string RESPONSE_END_SIGN = "<!--END-->";
+        private const char DATA_SEPARATOR = ';';
+
+        private static string strOutput = string.Empty;
+        private static string strLatency = "Latency";
+        private static DateTime dtLastUpdate = DateTime.Now;
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            timerUpdate.Start();
+        }
+
+        private void UpdateTranslation()
+        {
+            // Put data into objects
+            HTTP.PostData pdType = new HTTP.PostData("type", "send");
+            HTTP.PostData pdData = new HTTP.PostData("data", StringTools.Base64Encode(textBoxInput.Text));
+
+            // Get current time
+            DateTime dtSendTime = DateTime.Now;
+            // Send the request and process the output
+            string strOutputTemp = string.Join("\n", ExtractData(HTTP.GetPostResponse(SERVER_URL, pdType, pdData)));
+            // Calculate the server response latency
+            TimeSpan tsLatency = (DateTime.Now - dtSendTime);
+
+            string strLatencyTemp = "Latency: " + tsLatency.TotalMilliseconds.ToString("0") + " ms";
+
+            // Make sure to use the most recent data
+            // When data from older request than the last handled one returns ignore it
+            if (dtLastUpdate < dtSendTime)
+            {
+                dtLastUpdate = dtSendTime;
+
+                strOutput = strOutputTemp;
+                strLatency = strLatencyTemp;
+            }
+        }
+
+        private string[] ExtractData(string strResponse)
+        {
+            // Get the part of response before the end sign
+            string strData = strResponse.Substring(0, strResponse.IndexOf(RESPONSE_END_SIGN));
+            // Decode response string from Base64
+            string strDecoded = StringTools.Base64Decode(strData);
+            // Split the data by separator character and return the resulting string array
+            return strDecoded.Split(new char[] { DATA_SEPARATOR }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        private void timerUpdate_Tick(object sender, EventArgs e)
+        {
+            textBoxOutput.Text = strOutput;
+            labelLatency.Text = strLatency;
+        }
+
+        private void textBoxInput_TextChanged(object sender, EventArgs e)
+        {
+            Task.Run(() => UpdateTranslation());
+        }
+    }
+}
